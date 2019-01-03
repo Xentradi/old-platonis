85994403e0ebbadb2d7dfd1111cfa09485a8a715
Xentradi <ikaika@xentradi.com>
2019-01-03 11:41:51 -1000

    log member joins

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,7 +1,5 @@
 module.exports = (client, guildMember) => {
-	const lotusChildren = '517875377494818826';
-	const theGuild = guildMember.guild;
-	if (theGuild.id !== lotusChildren) return;
-	const uvRole = theGuild.roles.find(x => x.name === 'uv');
-	guildMember.addRole(uvRole.id);
+	const member = guildMember;
+	const guild = member.guild;
+	client.logger.log('info', `${member} has joined ${guild}.`);
 };
